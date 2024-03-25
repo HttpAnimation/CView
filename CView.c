8b72243d37690a570fbeae6465f420b6fd6b4363
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-25 11:58:51 -0700

    new test

--- CView.c
+++ CView.c
@@ -1,86 +1,79 @@
-#include <stdio.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include <webkit2/webkit2.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        fprintf(stderr, "SDL initialization failed: %s\n", SDL_GetError());
         return -1;
     }
 
-    // Create window
-    SDL_Window* window = SDL_CreateWindow("Webpage Viewer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (window == NULL) {
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    // Create a SDL window
+    SDL_Window *window = SDL_CreateWindow("SDL Webview", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                          800, 600, SDL_WINDOW_SHOWN);
+    if (!window) {
+        fprintf(stderr, "Window creation failed: %s\n", SDL_GetError());
         return -1;
     }
 
-    // Create renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+    // Create a SDL renderer
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        fprintf(stderr, "Renderer creation failed: %s\n", SDL_GetError());
         return -1;
     }
 
-    // Load image
-    SDL_Surface* surface = IMG_Load("index.html");
-    if (surface == NULL) {
-        printf("Unable to load image! SDL_Error: %s\n", SDL_GetError());
+    // Create a GTK WebKit WebView
+    GtkWidget *webview = webkit_web_view_new();
+    if (!webview) {
+        fprintf(stderr, "Webview creation failed\n");
         return -1;
     }
 
-    // Create texture from surface pixels
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (texture == NULL) {
-        printf("Unable to create texture from surface! SDL_Error: %s\n", SDL_GetError());
-        return -1;
-    }
+    // Load HTML content
+    webkit_web_view_load_html(WebKitWebView(webview),
+        "<!DOCTYPE html>"
+        "<html lang=\"en\">"
+        "<head>"
+        "    <meta charset=\"UTF-8\">"
+        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+        "    <title>SDL Webview</title>"
+        "</head>"
+        "<body>"
+        "    <h1>Hello, SDL Webview!</h1>"
+        "</body>"
+        "</html>", NULL);
 
-    // Free surface
-    SDL_FreeSurface(surface);
+    // Create a GTK widget for SDL window
+    GtkWidget *sdl_socket = gtk_socket_new();
+    gtk_widget_set_size_request(sdl_socket, 800, 600);
+    gtk_container_add(GTK_CONTAINER(sdl_socket), webview);
+    gtk_widget_realize(sdl_socket);
+    GdkWindow *sdl_window = gtk_widget_get_window(sdl_socket);
 
-    // Main loop flag
-    int quit = 0;
+    // Set the SDL window handle
+    SDL_SysWMinfo info;
+    SDL_VERSION(&info.version);
+    SDL_GetWindowWMInfo(window, &info);
+    gdk_window_reparent(sdl_window, info.info.x11.window, 0, 0);
 
-    // Event handler
-    SDL_Event e;
-
-    // Main loop
-    while (!quit) {
-        // Handle events on queue
-        while (SDL_PollEvent(&e) != 0) {
-            // User requests quit
-            if (e.type == SDL_QUIT) {
-                quit = 1;
+    // Main event loop
+    SDL_Event event;
+    int running = 1;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
             }
         }
-
-        // Clear screen
-        SDL_RenderClear(renderer);
-
-        // Render texture to screen
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
-
-        // Update screen
-        SDL_RenderPresent(renderer);
+        SDL_Delay(10);
     }
 
-    // Destroy texture
-    SDL_DestroyTexture(texture);
-    texture = NULL;
-
-    // Destroy renderer
+    // Cleanup
+    webkit_web_view_load_html(WebKitWebView(webview), "", NULL);
+    gtk_widget_destroy(sdl_socket);
     SDL_DestroyRenderer(renderer);
-    renderer = NULL;
-
-    // Destroy window
     SDL_DestroyWindow(window);
-    window = NULL;
-
-    // Quit SDL subsystems
     SDL_Quit();
-
     return 0;
 }
